a7c3aab4280a4da11c0ffce20c8bbe52861b8eb1
Ramalingappa <divya.ramalingappa@accenture.com>
2023-03-18 15:49:26 +0530

    Updated

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function myFunction(...args) {
-    console.log(args)
+let person = {
+   firstName: "Divya",
+   lastName: "Hello wide",
 }
 
-myFunction(1,2)
+const copiedperson1 = person;
+console.log(copiedperson1);
+
+const copy = Object.assign({}, person);
+console.log(copy);
+
+
+const copying = JSON.parse(JSON.stringify(person));
+console.log(copying)
